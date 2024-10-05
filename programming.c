c1eeafcbd63f03a915151979929a9caa1b539f5a
Osmangonibinismail <mdosmangoni4314@gmail.com>
2024-10-05 23:23:28 +0600

    arremetic oparetors

---
+++ programming.c
@@ -0,0 +1,81 @@
+#include <stdio.h>
+#include <math.h>
+
+// int main()
+// {
+
+//     printf("%d", 36+56);
+//     return 0;
+// }
+// int main()
+// {
+
+//     printf("%d", 36-56);
+//     return 0;
+// }
+// int main()
+// {
+
+//     printf("%d", 56*56);
+//     return 0;
+// }
+// int main()
+// {
+
+//     printf("%d", 56/56);
+//     return 0;
+// }
+// int main()
+// {
+
+//     printf("%f", 56.0 - 56.0);
+//     return 0;
+// }
+// int main()
+// {
+
+//     printf("%d", 56%56);
+//     return 0;
+// }
+// int main()
+// {
+//     float a = 6.3, b = 9;
+//     printf("%.2f", a + b);
+//     return 0;
+// }
+// int main()
+// {
+//     float a = 6.3, b = 9;
+//     float result = a * b;
+//     printf("%.2f", result);
+//     return 0;
+// }
+// int main()
+// {
+//     int a = 100000, b = 100000;
+//     long long result = 1ll * a * b;
+//     printf("%lld", result);
+//     return 0;
+// }
+// int main()
+// {
+//     long long a = 100000, b = 100000;
+//     long long result = a * b;
+//     printf("%lld", result);
+//     return 0;
+// }
+// int main()
+// {
+//     int a = 100000;
+//     double b = 100000;
+//     long long result = a * b;
+//     printf("%lld", result);
+//     return 0;
+// }
+int main()
+{
+    int a, b, c;
+    scanf("%d %d %d", &a, &b, &c);
+    printf("%d", a+ b + c);
+    return 0;
+}
---
+++ tempCodeRunnerFile.c
@@ -0,0 +1,7 @@
+int main()
+// {
+//     int a = 100000, b = 100000;
+//     long long result = 1ll * a * b;
+//     printf("%lld", result);
+//     return 0;
+// }
