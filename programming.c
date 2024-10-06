54668bbf24652ace68a3c76afd8b8996509040a8
Osmangonibinismail <mdosmangoni4314@gmail.com>
2024-10-07 00:58:38 +0600

    Nested If Else

--- programming.c
+++ programming.c
@@ -344,7 +344,6 @@
 // {
 //     int a = 12;
 
-
 //     if(a < 10)
 //     {
 //         a += 100;
@@ -359,7 +358,6 @@
 // {
 //     int a = 9;
 
-
 //     if(a < 10)
 //     {
 //         a += 100;
@@ -374,7 +372,6 @@
 // int main()
 // {
 //     int a = 9;
-
 
 //     if(a < 10)
 //     {
@@ -392,7 +389,6 @@
 // int main()
 // {
 //     int a = 12;
-
 
 //     if(a < 10)
 //     {
@@ -446,17 +442,102 @@
 //     }
 // }
 
-#include<stdio.h>
+// #include<stdio.h>
+// int main()
+// {
+//     int a = 50;
+//     if(a <= 40)
+//     {
+//         printf("Buy 2 eggs\n");
+//     }
+//     else
+//     {
+//         printf("Buy 1 eggs\n");
+//         printf("give %d Taka", 50 - a);
+//     }
+// }
+
+// #include<stdio.h>
+
+// int main()
+// {
+//     int a;
+//     scanf("%d", &a);
+//     if(a < 5)
+//     {
+//         printf("a is less than 5\n");
+//     }
+//     else if(a < 10)
+//     {
+//         printf("a is less then 10\n");
+//     }
+//     else if(a < 20)
+//     {
+//         printf("a is less then 20\n");
+//     }
+//     else if(a < 30)
+//     {
+//         printf("a is less then 30\n");
+//     }
+//     else if(a < 30)
+//     {
+//         printf("a is less then 30\n");
+//     }
+//     else if(a < 40)
+//     {
+//         printf("a is less then 40\n");
+//     }
+//     else if(a < 50)
+//     {
+//         printf("a is less then 50\n");
+//     }
+//     else if(a < 100)
+//     {
+//         printf("a is less then 100\n");
+//     }
+//     else if(a < 200)
+//     {
+//         printf("a is less then 200\n");
+//     }
+//     else
+//     {
+//         printf("a is less then 200+\n");
+//     }
+
+//     printf("this is final number");
+// }
+
+#include <stdio.h>
+#include <stdbool.h>
 int main()
 {
-    int a = 50;
-    if(a <= 40)
+    int tourDays = 1;
+    if (tourDays == 2)
     {
-        printf("Buy 2 eggs\n");
+        bool isBoatAvailable = false;
+        if(isBoatAvailable)
+        {
+            printf("going to cox's bazar\n");
+        }
+        else
+        printf("not going  to cox's bazar");
     }
-    else
+    else if(tourDays == 4)
     {
-        printf("Buy 1 eggs\n");
-        printf("give %d Taka", 50 - a);
+        bool shipAvailable = true;
+        if(shipAvailable)
+        {
+            printf("going to saint martin\n");
+        }
+        else
+        {
+            printf("not going to saint martin");
+        }
     }
+    else{
+        printf("kotaw jabo na");
+    }
+
+    return 0;
+
 }
