730ef1052fd684ba8e55de2a253d05505cc1934e
Osmangonibinismail <mdosmangoni4314@gmail.com>
2024-10-11 23:43:53 +0600

    hi hello

--- programming.c
+++ programming.c
@@ -764,3 +764,21 @@
 //         printf("no");
 //     }
 // }
+#include <stdio.h>
+int main()
+{
+    int x = 5;
+    if (x < 10)
+    {
+        printf("hello ");
+    }
+    if (x == 5)
+    {
+        printf("hi");
+    }
+    else
+    {
+        printf("no");
+    }
+}
+
